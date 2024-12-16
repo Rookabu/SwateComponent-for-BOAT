ea6f1dcbb546362ba7e31f47133cec0dbb1f2f8c
Rookabu <annika-paul1@gmx.de>
2024-12-16 17:12:21 +0100

    try implement swate input

--- src/Components.fs
+++ src/Components.fs
@@ -3,6 +3,7 @@ namespace App
 open Feliz
 open Feliz.Bulma
 open ARCtrl
+open Feliz.DaisyUI
 
 type Annotation = 
     {
@@ -20,14 +21,45 @@ type Annotation =
         }
     member this.ToggleOpen () = {this with IsOpen = not this.IsOpen}
 
+type Model = {
+    ///PageState
+    // PageState                   : PageState
+    // ///Data that needs to be persistent once loaded
+    // PersistentStorageState      : PersistentStorageState
+    // ///Error handling, Logging, etc.
+    // DevState                    : DevState
+    // ///States regarding term search
+    // TermSearchState             : TermSearch.Model
+    // ///Use this in the future to model excel stuff like table data
+    // ExcelState                  : OfficeInterop.Model
+    // ///States regarding File picker functionality
+    // FilePickerState             : FilePicker.Model
+    // ProtocolState               : Protocol.Model
+    // ///Insert annotation columns
+    AddBuildingBlockState       : BuildingBlock.Model
+    // CytoscapeModel              : Cytoscape.Model
+    // ///
+    // DataAnnotatorModel          : DataAnnotator.Model
+    // /// Contains all information about spreadsheet view
+    // SpreadsheetModel            : Spreadsheet.Model
+    // History                     : LocalHistory.Model
+    // ModalState                  : ModalState
+}
+
 type Components =
+    
+
     [<ReactComponent>]
     static member annoBlockwithSwate() =
-    
+       
         let testAnno = Annotation.init(OntologyAnnotation("key1"), CompositeCell.createFreeText("value1"))
+
         let (annoState: Annotation list, setState) = React.useState ([testAnno])
         let revIndex = 0
         let a = annoState.[revIndex]
+
+        let (currentOnto: OntologyAnnotation option, ontoSetter) = React.useState(annoState[0].Key)
+
 
         let updateAnnotation (func:Annotation -> Annotation) =
             let nextA = func a
@@ -35,7 +67,45 @@ type Components =
                 if i = revIndex then nextA else a 
             ) |> setState
 
+        let SearchBuildingBlockHeaderElement (model: Model, dispatch) =
+            let state = model.AddBuildingBlockState
+            let element = React.useElementRef()
+
+            Html.div [
+                prop.ref element // The ref must be place here, otherwise the portalled term select area will trigger daisy join syntax
+                prop.style [style.position.relative]
+                prop.children [
+                    Daisy.join [
+                        prop.className "w-full"
+                        prop.children [
+                            // Choose building block type dropdown element
+                            // Dropdown building block type choice
+                            // Dropdown.Main(ui, setUi, model, dispatch)
+                            // Term search field
+                            if state.HeaderCellType.HasOA() then
+                                let setter (oaOpt: OntologyAnnotation option) =
+                                    let case = oaOpt |> Option.map (fun oa -> !^oa)
+                                    BuildingBlock.UpdateHeaderArg case |> BuildingBlockMsg |> dispatch
+                                    //selectHeader ui setUi h |> dispatch
+                                let input = model.AddBuildingBlockState.TryHeaderOA()
+                                Components.TermSearch.Input(setter, fullwidth=true, ?input=input, isjoin=true, ?portalTermSelectArea=element.current, classes="border-current")
+                            elif state.HeaderCellType.HasIOType() then
+                                Daisy.input [
+                                    prop.readOnly true
+                                    prop.valueOrDefault (
+                                        state.TryHeaderIO()
+                                        |> Option.get
+                                        |> _.ToString()
+                                    )
+                                ]
+                        ]
+                    ]
+                ]
+            ]
+
         Bulma.block [
+            prop.className "m-30"
+            prop.children [
             if a.IsOpen = false then 
                 Html.button [
                     Html.i [
@@ -53,6 +123,7 @@ type Components =
             else
                 Html.div [
                     prop.className "bg-[#ffe699] p-3 text-black z-50 max-w-96 mb-20"
+                    
                     prop.children [
                         Bulma.columns [
                             Bulma.column [
@@ -112,10 +183,12 @@ type Components =
                                             setState newAnnoList
                                         )
                                     ]
+                                    
                                 ]
                             ]
                         ]
                     ]  
                 ]
+            ]
         ]
 
